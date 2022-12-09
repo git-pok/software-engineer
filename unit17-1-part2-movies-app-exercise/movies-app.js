f36c30a77a34324309417f670fb637c1f1ced1de
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2022-12-09 10:39:32 -0500

    unit17-1-part2-movies-app-exercise/

---
+++ unit17-1-part2-movies-app-exercise/movies-app.js
@@ -0,0 +1,30 @@
+// When the form is submitted, capture the values for each of the inputs 
+// and append them to the DOM along with a button to remove each title 
+// and rating from the DOM.
+function movieRating(e) {
+    e.preventDefault();
+    const $titleValue = $('#title').val(); 
+    const $ratingValue = $('#rating').val();
+    $('.movies').append(
+        `<div>
+            <h1 class='review'>
+                <span class='upperCase'>Movie Title:</span> ${$titleValue} 
+                <span class='upperCase'>Rating:</span> ${$ratingValue}
+            </h1>
+            <button type='button'>Remove</button>
+        </div>`);
+    $('#title').val('')
+    $('#rating').val('');
+}
+
+$('form').on('submit', movieRating);
+
+// When the button to remove is clicked, 
+// remove each title and rating from the DOM.
+function removeReview(e) {
+    if (e.target.tagName === 'BUTTON') $(this).remove();
+}
+
+$('.movies').on('click', 'div', removeReview);
+
+
