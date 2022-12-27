f2785f2a7613d5d939328518d759d272057f2647
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2022-12-27 10:53:21 -0500

    added files

---
+++ unit20-section1-cumulative-project-hack-or-snooze/hack-or-snooze-ajax-api/js/nav.js
@@ -0,0 +1,49 @@
+"use strict";
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+  console.debug("navAllStories", evt);
+  hidePageComponents();
+  putStoriesOnPage();
+}
+
+$body.on("click", "#nav-all", navAllStories);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+  console.debug("navLoginClick", evt);
+  hidePageComponents();
+  $loginForm.show();
+  $signupForm.show();
+}
+
+$navLogin.on("click", navLoginClick);
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+  console.debug("updateNavOnLogin");
+  $(".main-nav-links").show();
+  $navLogin.hide();
+  $navLogOut.show();
+  $navUserProfile.text(`${currentUser.username}`).show();
+}
+
+/*ADDED CUMULATIVE CODE*/
+function showHideStoryForm() {
+  if ($('.show-hide').css('display') === 'none') {
+    // console.log('NONE');
+    $('.show-hide').show();
+  } else {
+    $('.show-hide').hide();
+  }
+}
+
+$body.on("click", ".navbar-submit", showHideStoryForm);
+/*END OF ADDED CUMULATIVE CODE*/
