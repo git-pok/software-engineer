211dfbf3123245ea1e228db05c6908092c6da11d
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-01-28 10:18:13 -0500

    made a new logic for: unit22-python-fundamentals/unit22-2-python-ds-practice-exercise/37_sum_up_diagonals

--- unit22-python-fundamentals/unit22-2-python-ds-practice-exercise/37_sum_up_diagonals/sum_up_diagonals.py
+++ unit22-python-fundamentals/unit22-2-python-ds-practice-exercise/37_sum_up_diagonals/sum_up_diagonals.py
@@ -2,6 +2,9 @@ def sum_up_diagonals(matrix):
     """Given a matrix [square list of lists], return sum of diagonals.
 
     Sum of TL-to-BR diagonal along with BL-to-TR diagonal:
+    # run these tests in the Terminal,
+    # not in python or ipython shell,
+    # run with, python3 -m doctest -v file_name.py
 
         >>> m1 = [
         ...     [1,   2],
@@ -17,17 +20,43 @@ def sum_up_diagonals(matrix):
         ... ]
         >>> sum_up_diagonals(m2)
         30
+
+        >>> m3 = [
+        ...    [1, 2, 3, 4],
+        ...    [5, 6, 7, 8],
+        ...    [9, 10, 11, 12],
+        ...    [13, 14, 15, 16],
+        ... ]
+        >>> sum_up_diagonals(m3)
+        68
     """
     total = 0
 
-    for i in range(len(matrix)):
-        total += matrix[i][i]
-        total += matrix[i][-1 - i]
-
+    for i in matrix:
+        if i == matrix[0]:
+            total += matrix[0][0]
+            total += matrix[0][-1] 
+        if i != matrix[0] and i != matrix[-1]:
+            total += i[1]
+            total += i[-2] 
+        if i == matrix[-1]:
+            total += matrix[-1][0]
+            total += matrix[-1][-1]
+        
     return total
 
 
-print("should return 73 ----->,", sum_up_diagonals([[1,   2], [30, 40],]))
-print("should return 30 ----->,", sum_up_diagonals([[1, 2, 3], [4, 5, 6], [7, 8, 9],]))
+# print("should return 73 ----->,", sum_up_diagonals([[1,   2], [30, 40],]))
+# print("should return 30 ----->,", sum_up_diagonals([[1, 2, 3], [4, 5, 6], [7, 8, 9],]))
+# print("should return 68 ----->,", sum_up_diagonals([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]))
+
+# Old Solution
+# # total = 0
+
+    # for i in range(len(matrix)):
+    #     total += matrix[i][i]
+    #     total += matrix[i][-1 - i]
+
+    # return total
 
 
