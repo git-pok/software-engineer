669ca2da0a8d926f70a9e4fcbd1f2b998f90c1e9
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-05-01 17:16:11 -0400

    Defined logic in PlayTable.js: state, useEffect, and props for drawing and tracking cards. Defined logic in DrawButton.js: function for drawing cards and setting parent state.

--- unit41-reactjs-fundamentals/41-9-react-use-effect-ref/card-deck/src/DrawButton.js
+++ unit41-reactjs-fundamentals/41-9-react-use-effect-ref/card-deck/src/DrawButton.js
@@ -1,9 +1,30 @@
+import { useState, useRef } from 'react';
+import axios from 'axios';
 import './DrawButton.css';
 
-const DrawButton = ({ deckId }) => {
+const DrawButton = ({ deck, currCard, currImage }) => {
+
+  const DRAW_URL = `
+  https://deckofcardsapi.com/api/deck/${deck}/draw/?count=1
+`;
+  // console.log("DECK ID", deck);
+  const drawCard = async (deck) => {
+    const res = await axios.get(DRAW_URL);
+    const cardsLeft = res.data.remaining;
+    console.log("CARDS REMIANING", cardsLeft);
+    if (cardsLeft === 0) alert("NO CARDS LEFT");
+    const { code } = res.data.cards[0];
+    const { image } = res.data.cards[0];
+  
+    currCard(code);
+    currImage(image);
+  }
+
   return (
     <>
-      <button className="DrawButton-button">DRAW CARD</button>
+      <button
+        className="DrawButton-button"
+        onClick={drawCard}>DRAW CARD</button>
     </>
   );
 }
--- unit41-reactjs-fundamentals/41-9-react-use-effect-ref/card-deck/src/PlayTable.js
+++ unit41-reactjs-fundamentals/41-9-react-use-effect-ref/card-deck/src/PlayTable.js
@@ -1,35 +1,64 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import './PlayTable.css';
 import DrawButton from './DrawButton.js';
 
 const SHUFFLE_URL = `
-    https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6
+    https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1
 `;
 
 const PlayTable = () => {
   const [ deckId, setDeckId ] = useState(null);
+  const [ card, setCard ] = useState(null);
+  const [ cardImage, setCardImage ] = useState();
+
+  console.log("CURR CARD", card);
+  console.log("CURR CARD IMAGE", cardImage);
+  const currCard = (code) => {
+    setCard(card => code);
+  }
+
+  const currImage = (image) => {
+    setCardImage(cardImage => image);
+  }
+
+  const cardDiv = useRef();
 
   useEffect(() => {
 
     async function ShuffleDeck() {
-      const res = await axios.get(SHUFFLE_URL);
-      setDeckId(data => res.data.deck_id);
+      try {
+        const res = await axios.get(SHUFFLE_URL);
+        setDeckId(data => res.data.deck_id);
+        // console.log("DATA", res.data);
+      } catch(err) {
+        throw new Error(`ERROR!!!: \n${err}`);
+      }
     }
 
-    try {
-      ShuffleDeck();
-    } catch(err) {
-      throw new Error(`ERROR: \n${err}`);
-    }
+    ShuffleDeck();
 
   }, []);
+
+  useEffect(() => {
+    if (card) cardDiv.current.src = cardImage;
+  }, [card])
 
   return (
     <>
     <h1 className="PlayTable-h1">Play Table</h1>
     <div className="PlayTable-div">
-      <DrawButton deck={deckId} />
+      <DrawButton
+        deck={deckId}
+        currCard={currCard}
+        currImage={currImage} />
+      { card &&
+        <img
+          ref={cardDiv}
+          className="PlayTable-card">
+        </img>
+      }
+      
     </div>
     </>
   );
