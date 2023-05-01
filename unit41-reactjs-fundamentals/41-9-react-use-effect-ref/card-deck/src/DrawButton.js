8bf164fdf476e744204ca9756a96794fc0493a86
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-05-01 16:07:13 -0400

    Defined logic in PlayTable.js: axios request to receive a shuffled deck, state for shuffled deck, and DrawButton prop. Defined logic in DrawButton.js: prop.

--- unit41-reactjs-fundamentals/41-9-react-use-effect-ref/card-deck/src/DrawButton.js
+++ unit41-reactjs-fundamentals/41-9-react-use-effect-ref/card-deck/src/DrawButton.js
@@ -1,6 +1,6 @@
 import './DrawButton.css';
 
-const DrawButton = () => {
+const DrawButton = ({ deckId }) => {
   return (
     <>
       <button className="DrawButton-button">DRAW CARD</button>
--- unit41-reactjs-fundamentals/41-9-react-use-effect-ref/card-deck/src/PlayTable.js
+++ unit41-reactjs-fundamentals/41-9-react-use-effect-ref/card-deck/src/PlayTable.js
@@ -1,12 +1,35 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './PlayTable.css';
 import DrawButton from './DrawButton.js';
 
+const SHUFFLE_URL = `
+    https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6
+`;
+
 const PlayTable = () => {
+  const [ deckId, setDeckId ] = useState(null);
+
+  useEffect(() => {
+
+    async function ShuffleDeck() {
+      const res = await axios.get(SHUFFLE_URL);
+      setDeckId(data => res.data.deck_id);
+    }
+
+    try {
+      ShuffleDeck();
+    } catch(err) {
+      throw new Error(`ERROR: \n${err}`);
+    }
+
+  }, []);
+
   return (
     <>
     <h1 className="PlayTable-h1">Play Table</h1>
     <div className="PlayTable-div">
-      <DrawButton />
+      <DrawButton deck={deckId} />
     </div>
     </>
   );
