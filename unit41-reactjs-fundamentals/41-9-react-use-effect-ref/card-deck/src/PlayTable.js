e20231f2eaadefb0ac59f639a3fe212dc114a8ee
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-05-01 15:38:12 -0400

    Created components in PlayTable.js: DrawButton. Created new file: DrawButton.css.

--- unit41-reactjs-fundamentals/41-9-react-use-effect-ref/card-deck/src/PlayTable.js
+++ unit41-reactjs-fundamentals/41-9-react-use-effect-ref/card-deck/src/PlayTable.js
@@ -1,10 +1,12 @@
 import './PlayTable.css';
+import DrawButton from './DrawButton.js';
 
 const PlayTable = () => {
   return (
     <>
     <h1 className="PlayTable-h1">Play Table</h1>
     <div className="PlayTable-div">
+      <DrawButton />
     </div>
     </>
   );
