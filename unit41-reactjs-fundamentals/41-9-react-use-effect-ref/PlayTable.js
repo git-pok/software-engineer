75a67c054297f732e363a64d9782d1d61489dfe0
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-05-01 15:11:38 -0400

    Added jsx elements to PlayTable.js: div, h1. Added classes to jsx elements in PlayTable.js: PlayTable-div, PlayTable-h1.

--- unit41-reactjs-fundamentals/41-9-react-use-effect-ref/PlayTable.js
+++ unit41-reactjs-fundamentals/41-9-react-use-effect-ref/PlayTable.js
@@ -3,7 +3,9 @@ import './PlayTable.css';
 const PlayTable = () => {
   return (
     <>
-      <h1>Play Table</h1>
+    <h1 className="PlayTable-h1">Play Table</h1>
+    <div className="PlayTable-div">
+    </div>
     </>
   );
 }
