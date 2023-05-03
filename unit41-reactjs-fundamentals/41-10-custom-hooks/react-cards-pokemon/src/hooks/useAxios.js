5ebaf3c1abd65bfc8da2f5dee05dce03ff424dff
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-05-03 11:47:23 -0400

    Defined logic in useAxios.js: defined custom hook for PlayingCardList.js axios request.

--- unit41-reactjs-fundamentals/41-10-custom-hooks/react-cards-pokemon/src/hooks/useAxios.js
+++ unit41-reactjs-fundamentals/41-10-custom-hooks/react-cards-pokemon/src/hooks/useAxios.js
@@ -1,13 +1,25 @@
 import { useState } from 'react';
+import axios from "axios";
 
-const useFlip = () => {
-  const [ state, setState ] = useState(true);
 
-  const toggleState = () => {
-    setState(state => !state);
-  }
+const useAxios = (url) => {
+  const [ state, setState ] = useState(() => {
+    useEffect(() => {
+      const addApiData = async (url) => {
+        try {
+          const response = await axios.get(url);
+          setState(state => [...state, { ...response.data, id: uuid() }]);
+        } catch(err) {
+          throw new Error(`ERROR!!! \n${err}`);
+        }
+      }
+  
+      addApiData();
+  
+    }, [url])
+  });
 
-  return [ state, toggleState ];
+  return [ state, setState ];
 }
 
-export default useFlip;
+export default useAxios;
