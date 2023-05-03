a8447aadb32e6a01e856d331dea826acf3f3fd2f
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-05-03 12:20:41 -0400

    Commented and implemented useAxios in PlayingCardList.js.

--- unit41-reactjs-fundamentals/41-10-custom-hooks/react-cards-pokemon/src/hooks/useAxios.js
+++ unit41-reactjs-fundamentals/41-10-custom-hooks/react-cards-pokemon/src/hooks/useAxios.js
@@ -1,25 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from "axios";
+import {v4 as uuid} from 'uuid';
 
 
 const useAxios = (url) => {
-  const [ state, setState ] = useState(() => {
-    useEffect(() => {
-      const addApiData = async (url) => {
-        try {
-          const response = await axios.get(url);
-          setState(state => [...state, { ...response.data, id: uuid() }]);
-        } catch(err) {
-          throw new Error(`ERROR!!! \n${err}`);
-        }
-      }
-  
-      addApiData();
-  
-    }, [url])
-  });
+  const [ state, setState ] = useState([]);
 
-  return [ state, setState ];
+  const addCardApiData = async () => {
+    // url will be undefined if passed into
+    // the async function's arguments.
+    try {
+      const response = await axios.get(url);
+      setState(state => [...state, { ...response.data, id: uuid() }]);
+    } catch(err) {
+      throw new Error(`ERROR!!! \n${err}`);
+    }
+  }
+
+  return [ state, addCardApiData ];
 }
 
 export default useAxios;
