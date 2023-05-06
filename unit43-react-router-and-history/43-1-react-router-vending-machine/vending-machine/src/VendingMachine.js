b3ca8de56b3fa49d548ec71f2393c7dad7b306ef
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-05-06 16:23:28 -0400

    Edited VendingMachine.js: passed props into react router components.

--- unit43-react-router-and-history/43-1-react-router-vending-machine/vending-machine/src/VendingMachine.js
+++ unit43-react-router-and-history/43-1-react-router-vending-machine/vending-machine/src/VendingMachine.js
@@ -8,24 +8,25 @@ const VendingMachine = () => {
     <BrowserRouter>
       <nav className="VendingMachine-nav">
         <NavLink exact to="/">HOME</NavLink>
-        <NavLink exact to="/SNACKI">SNACKI</NavLink>
-        <NavLink exact to="/SNACKII">SNACKII</NavLink>
+        <NavLink exact to="/mambas">Mambas</NavLink>
+        <NavLink exact to="/pretzels">Pretzels</NavLink>
+        <NavLink exact to="/twix">Twix</NavLink>
       </nav>
 
       <Route exact path="/">
-        <Snack />
+        <img src="https://i.giphy.com/media/VGDt57CysJa68fQ7Rd/giphy.webp"></img>
       </Route>
 
-      <Route exact path="/">
-        <Snack />
+      <Route exact path="/mambas">
+        <Snack name="Mambas" img="https://media0.giphy.com/media/W36qRrUjqYenJQ51nB/giphy.gif?cid=ecf05e47zjs8ey9y0mwa0pivbrti4kpikkbaau6riy0d8fec&ep=v1_gifs_search&rid=giphy.gif&ct=g" />
       </Route>
 
-      <Route exact path="/">
-        <Snack />
+      <Route exact path="/pretzels">
+        <Snack name="pretzels" img="https://media4.giphy.com/media/XulKijZI11Hx8NA4mR/giphy.gif?cid=ecf05e47wknguiw70b33emgrd1s5rxin6xy3j7qjfs5ftlgx&ep=v1_gifs_search&rid=giphy.gif&ct=g" />
       </Route>
 
-      <Route exact path="/">
-        <Snack />
+      <Route exact path="/twix">
+        <Snack name="Twix" img="https://i.giphy.com/media/ei9pMw3x3MYVhtlBla/giphy.webp" />
       </Route>
 
     </BrowserRouter>
