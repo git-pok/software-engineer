8df53a4bcddc279f8f0c0a8cd25cc6564bf19017
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-05-06 14:52:49 -0400

    Edited VendingMachine.js: defined returned jsx..

--- unit43-react-router-and-history/43-1-react-router-vending-machine/vending-machine/src/VendingMachine.js
+++ unit43-react-router-and-history/43-1-react-router-vending-machine/vending-machine/src/VendingMachine.js
@@ -1,8 +1,32 @@
+import { BrowserRouter, Route, NavLink } from 'react-router-dom@5.2.1';
+import Snack from './Snack.js';
 import './VendingMachine.css';
 
 const VendingMachine = () => {
   return (
     <>
+    <BrowserRouter>
+      <nav>
+        <NavLink />
+      </nav>
+
+      <Route exact path="/">
+        <Snack />
+      </Route>
+
+      <Route exact path="/">
+        <Snack />
+      </Route>
+
+      <Route exact path="/">
+        <Snack />
+      </Route>
+
+      <Route exact path="/">
+        <Snack />
+      </Route>
+
+    </BrowserRouter>
     <h1>VendingMachine COMPONENT</h1>
     </>
   );
