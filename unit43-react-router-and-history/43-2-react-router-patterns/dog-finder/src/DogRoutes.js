d43b38b10258b0ee404a0f8a950541b37b69fb5b
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-05-09 18:23:24 -0400

    Edited /src/DogRoutes.js: defined Redirect route for invalid routes.

--- unit43-react-router-and-history/43-2-react-router-patterns/dog-finder/src/DogRoutes.js
+++ unit43-react-router-and-history/43-2-react-router-patterns/dog-finder/src/DogRoutes.js
@@ -1,10 +1,10 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import DogProfile from './DogProfile.js';
 import DogList from './DogList.js';
 // import './DogNav.css';
 
 const DogRoutes = ({ props }) => {
- 
+
   return (
     <Switch>
         <Route exact path="/dogs">
@@ -14,6 +14,8 @@ const DogRoutes = ({ props }) => {
         <Route exact path="/dogs/:name">
             <DogProfile props={props} />
         </Route>
+
+        <Redirect to="/dogs" />
     </Switch>
   );
 }
