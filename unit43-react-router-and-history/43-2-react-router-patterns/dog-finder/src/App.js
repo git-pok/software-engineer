a8f2a75e09877bdf9d7ec4afdb8bf967b756f187
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-05-09 10:43:31 -0400

    Edited App.js: defined default props.

--- unit43-react-router-and-history/43-2-react-router-patterns/dog-finder/src/App.js
+++ unit43-react-router-and-history/43-2-react-router-patterns/dog-finder/src/App.js
@@ -1,9 +1,56 @@
 import './App.css';
 
-const App = () => {
+const App = (props) => {
+  console.log(props);
   return (
     <h1>APP COMPONENT</h1>
   );
 }
+
+App.defaultProps = {
+  dogs: [
+    {
+      name: "Whiskey",
+      age: 5,
+      src: "",
+      facts: [
+        "Whiskey loves eating popcorn.",
+        "Whiskey is a terrible guard dog.",
+        "Whiskey wants to cuddle with you!"
+      ]
+    },
+    {
+      name: "Duke",
+      age: 3,
+      src: "",
+      facts: [
+        "Duke believes that ball is life.",
+        "Duke likes snow.",
+        "Duke enjoys pawing other dogs."
+      ]
+    },
+    {
+      name: "Perry",
+      age: 4,
+      src: "",
+      facts: [
+        "Perry loves all humans.",
+        "Perry demolishes all snacks.",
+        "Perry hates the rain."
+      ]
+    },
+    {
+      name: "Tubby",
+      age: 4,
+      src: "",
+      facts: [
+        "Tubby is really stupid.",
+        "Tubby does not like walks.",
+        "Angelina used to hate Tubby, but claims not to anymore."
+      ]
+    }
+  ]
+}
+// console.log(App);
 
 export default App;
