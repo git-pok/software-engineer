f617321e82301235437dba1a64e4c0e116a5b3bd
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-05-10 13:27:20 -0400

    Uninstalled dependency: react-router-dom. Installed dependency: react-router-dom@5.2.1.

--- unit43-react-router-and-history/43-2-react-router-patterns/color-factory/src/App.js
+++ unit43-react-router-and-history/43-2-react-router-patterns/color-factory/src/App.js
@@ -1,12 +1,13 @@
 import { 
   BrowserRouter
 } from 'react-router-dom';
+import ColorRoutes from './ColorRoutes.js';
 import './App.css';
 
 const App = () => {
   return (
     <BrowserRouter>
-    
+      <ColorRoutes />
     </BrowserRouter>
   );
 }
