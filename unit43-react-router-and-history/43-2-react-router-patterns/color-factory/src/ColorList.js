9c4714d435366af3dcf2733b17ee8941989fe37e
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-05-10 14:24:53 -0400

    Edited /src/ColorList.js: defined exact prop.

--- unit43-react-router-and-history/43-2-react-router-patterns/color-factory/src/ColorList.js
+++ unit43-react-router-and-history/43-2-react-router-patterns/color-factory/src/ColorList.js
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 const ColorList = (props) => {
     console.log(props);
     return (
-        props.map(val => (
-            <div className="ColorList-content-container">
+        props.dogs.map((val, idx) => (
+            <div
+                key={idx}
+                className="ColorList-content-container">
                 <div
                     className="ColorList-content"
                     style={{backgroundColor: val.color}}>
                     <div className="ColorList-text">
-                        <Link exact to={`/colors/${val.color}`}>
+                        <Link exact="true" to={`/colors/${val.color}`}>
                            {val.color} 
                         </Link>
                     </div>
@@ -19,8 +21,10 @@ const ColorList = (props) => {
     );
 }
 
-ColorList.defaultProps = [
-    {color: "red"}
-]
+ColorList.defaultProps = { 
+    dogs: [
+        {color: "red"}
+    ]
+}
 
 export default ColorList; 
