d7ea66ec1eacf38fb4141f15109f971724592e22
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-05-11 12:07:45 -0400

    Edited ColorList.css: defined styles for ColorDetail.

--- unit43-react-router-and-history/43-2-react-router-patterns/color-factory/src/ColorDetail.js
+++ unit43-react-router-and-history/43-2-react-router-patterns/color-factory/src/ColorDetail.js
@@ -1,10 +1,15 @@
 import {useParams} from 'react-router-dom';
+import './ColorDetail.css';
 
 const ColorDetail = () => {
     const { color } = useParams();
 
     return (
-        <h1>{color}</h1>
+        <div
+            style={{backgroundColor: `#${color}`}}
+            className="ColorDetail">
+            <h1>Color: {`#${color}`}</h1>
+        </div>
     );
 }
 
