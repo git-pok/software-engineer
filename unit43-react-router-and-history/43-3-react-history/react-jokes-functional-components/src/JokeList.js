e2aeec75c1eaede63e344ffaf8c8822846d471ca
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-05-15 17:20:37 -0400

    Edited JokeList.js: changed while loop to infinite loop.

--- unit43-react-router-and-history/43-3-react-history/react-jokes-functional-components/src/JokeList.js
+++ unit43-react-router-and-history/43-3-react-history/react-jokes-functional-components/src/JokeList.js
@@ -5,7 +5,7 @@ import "./JokeList.css";
 
 
 const JokeList = ({ numJokesToGet }) => {
-
+    // const [ jokeIds, setJokeIds ] = useState({});
     const [ jokes, setJokes ] = useState(null);
     const [ isLoading, setIsLoading ] = useState(false);
     const [ req, setReq ] = useState(false);
@@ -17,13 +17,22 @@ const JokeList = ({ numJokesToGet }) => {
         ))
     }
 
+    // const addJokeIds = id => {
+    //   setJokeIds(jokeIds => ({
+    //     ...jokeIds,
+    //     id
+    //   }))
+    // }
+
     const reqTrack = () => {
-        setReq(() => {
+        setReq(req => { 
+        // {
         if (!req) return 1;
         else if (req === 1) return 2;
         else return 1; 
-        });  
-    }
+        // });  
+    })
+  }
 
     useEffect(() => {
 
@@ -32,38 +41,51 @@ const JokeList = ({ numJokesToGet }) => {
             if (!req) return;
             else {
                 setJokes(jokes => null);
-                let jokeDataJokes = 0;
+                // let jokeDataJokes = 0;
                 const jokesData = [];
                 const jokeIds = {};
+                // const jokeIds = [];
+                setIsLoading(() => true);
 
-                while (jokeDataJokes < numJokesToGet) {
-                    setIsLoading(() => true);
-                    const res = await axios.get("https://icanhazdadjoke.com", {
-                        headers: { Accept: "application/json" }
-                    });
+                for (let i = 0; i < 6; i++) {
+                  const res = await axios.get("https://icanhazdadjoke.com", {
+                      headers: { Accept: "application/json" }
+                  });
 
-                    const jokeId = res.data.id;
+                  const jokeId = res.data.id;
+                  // jokeIds[jokeId] = jokeId;
+                  // console.log("JOKE", res.data.joke);
+                  // console.log("JOKE ID", jokeIds.jokeId);
+                  // console.log(jokeIds.indexOf(jokeId));
+                  // if (jokeIds.indexOf(jokeId) === -1) {
+                    if (jokeIds.jokeId === undefined) {
+                    jokesData.push({
+                        id: res.data.id,
+                        joke: res.data.joke,
+                        votes: 0
+                    })
+
+                    // addJokeIds(res.data.id);
+                    // jokeIds.push(jokeId);
                     jokeIds[jokeId] = jokeId;
+                    // addJokes(jokesData);
+                  }
 
-                    if (!jokeIds.jokeId) {
-
-                        jokesData.push({
-                            id: res.data.id,
-                            joke: res.data.joke,
-                            votes: 0
-                        })
-
-                        jokeDataJokes++;
-                    }
+                  // jokeIds[jokeId] = jokeId;
+                  // console.log(jokesData);
+                  console.log("JOKE IDS", jokeIds);
+                  if (jokesData.length === 5) break;
                 }
 
+                console.log("JOKES DATA", jokesData);
                 addJokes(jokesData);
                 setIsLoading(() => false);
 
             }
       
           } catch (err) {
-            console.error(err);
+            console.error(`ERROR!!!\n${err}`);
+            window.location.reload();
           }
         }
 
@@ -92,7 +114,6 @@ const JokeList = ({ numJokesToGet }) => {
                       key={j.id}
                       id={j.id}
                       votes={j.votes} />
-            //   vote={this.vote}
                   )) 
                 : 
                   null 
