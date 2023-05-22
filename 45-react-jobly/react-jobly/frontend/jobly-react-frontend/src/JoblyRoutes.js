fc346e51f20ce22369c8165144487e0ccd896bdd
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-05-22 16:50:31 -0400

    Edited /frontend/jobly-react-frontend/src/JoblyRoutes.js: defined switch and routes.

---
+++ 45-react-jobly/react-jobly/frontend/jobly-react-frontend/src/JoblyRoutes.js
@@ -0,0 +1,34 @@
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+const JoblyRoutes = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+
+      </Route>
+      <Route exact path="/companies">
+
+      </Route>
+      <Route exact path="/jobs">
+
+      </Route>
+      <Route exact path="/users">
+
+      </Route>
+      <Route exact path="/companies/:handle">
+
+      </Route>
+      <Route exact path="/jobs/:id">
+
+      </Route>
+      <Route exact path="/users/:username">
+
+      </Route>
+      <Redirect exact path="/">
+
+      </Redirect>
+    </Switch>
+  );
+}
+
+export default JoblyRoutes;
