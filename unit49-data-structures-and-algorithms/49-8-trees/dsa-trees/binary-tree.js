d42dad05c0f94d978ab03d20f0fc20da84c70967
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-07-05 16:24:12 -0400

    Edited binary-tree.js: refactored minDepth.

--- unit49-data-structures-and-algorithms/49-8-trees/dsa-trees/binary-tree.js
+++ unit49-data-structures-and-algorithms/49-8-trees/dsa-trees/binary-tree.js
@@ -1,21 +1,21 @@
 /** OG BinaryTreeNode: node for a general tree. */
-// class BinaryTreeNode {
-//   constructor(val, left = null, right = null) {
-//     this.val = val;
-//     this.left = left;
-//     this.right = right;
-//   }
-// }
-
 class BinaryTreeNode {
-  constructor(branch, val, left, right, children=[]) {
-    this.branch = branch;
+  constructor(val, left = null, right = null) {
     this.val = val;
     this.left = left;
     this.right = right;
-    this.children = children;   
   }
 }
+
+// class BinaryTreeNode {
+//   constructor(branch, val, left, right, children=[]) {
+//     this.branch = branch;
+//     this.val = val;
+//     this.left = left;
+//     this.right = right;
+//     this.children = children;   
+//   }
+// }
 
 class BinaryTree {
   constructor(root = null) {
@@ -55,35 +55,50 @@ class BinaryTree {
   /** minDepth()
    * return the minimum depth of the tree -- that is,
    * the length of the shortest path from the root to a leaf. */
-  minDepth() {
-    if (!this.root.val) return 0;
-    const toIterateQueue = [this.root];
-    let lowestBranch;
-    while (toIterateQueue.length) {
-      const current = toIterateQueue.shift();
-      // console.log("CURRENT", current);
-      if (!current.children.length) return lowestBranch = current.branch + 1;
-      for (let child of current.children) {
-        toIterateQueue.push(child);
-      }
-      lowestBranch = current.branch;
-    }
-    return lowestBranch;
-  }
-
   // minDepth() {
   //   if (!this.root.val) return 0;
   //   const toIterateQueue = [this.root];
-  //   let lowestBranch = 0;
+  //   let lowestBranch;
   //   while (toIterateQueue.length) {
   //     const current = toIterateQueue.shift();
-  //     console.log("CURRENT", current);
-  //     if (!current.left || !current.right) return lowestBranch;
-  //     toIterateQueue.push(current.left, current.right);
-  //     if (current.left && current.right) lowestBranch++;
+  //     // console.log("CURRENT", current);
+  //     if (!current.children.length) return lowestBranch = current.branch + 1;
+  //     for (let child of current.children) {
+  //       toIterateQueue.push(child);
+  //     }
+  //     lowestBranch = current.branch;
   //   }
   //   return lowestBranch;
   // }
+  /* RIGHT BINARY TREE NODE CLASS minDepth */
+  minDepth() {
+    if (!this.root) return 1;
+    const toIterateQueue = [this.root];
+    let branch = 1;
+    const branchDepths = [];
+    while (toIterateQueue.length) {
+      const current = toIterateQueue.pop();
+      console.log("toIterateQueue", toIterateQueue);
+      console.log("CURRENT", current);
+      console.log("branch", branch);
+      if ((!current.left || !current.right) && !toIterateQueue.length) {
+        branchDepths.push(branch);
+      } else if (!current.left || !current.right) {
+        console.log("POPPED");
+        // console.log("toIterateQueue", toIterateQueue);
+        toIterateQueue.pop();
+        console.log("toIterateQueue", toIterateQueue);
+        branchDepths.push(branch);
+        branch--;
+        console.log("branchDepths", branchDepths);
+      } else {
+        branch++;
+      }
+      if (current.left) toIterateQueue.push(current.left);
+      if (current.right) toIterateQueue.push(current.right);
+    }
+    return Math.min(...branchDepths);
+  }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
@@ -187,13 +202,13 @@ class BinaryTree {
 // const treeNeg = new BinaryTree(newTreeNeg);
 
 /* SAMPLE TREES */
-// const E = new Node(1);
-// const A = new Node(2);
-// const B = new Node(3);
-// const C = new Node(4);
-// const D = new Node(5);
-// const F = new Node(6);
-// const G = new Node(7);
+// const E = new BinaryTreeNode(1);
+// const A = new BinaryTreeNode(2);
+// const B = new BinaryTreeNode(3);
+// const C = new BinaryTreeNode(4);
+// const D = new BinaryTreeNode(5);
+// const F = new BinaryTreeNode(6);
+// const G = new BinaryTreeNode(7);
 // E.left = A;
 // E.right = B;
 // B.left = C;
@@ -201,17 +216,17 @@ class BinaryTree {
 // A.left = F;
 // const tree = new BinaryTree(E);
 
-// const one = new Node(1);
-// const two = new Node(2);
-// const three = new Node(3);
-// const four = new Node(4);
-// const five = new Node(5);
-// const six = new Node(6);
-// const seven = new Node(7);
-// const eight = new Node(8);
-// const nine = new Node(9);
-// const ten = new Node(10);
-// const elvn = new Node(11);
+// const one = new BinaryTreeNode(1);
+// const two = new BinaryTreeNode(2);
+// const three = new BinaryTreeNode(3);
+// const four = new BinaryTreeNode(4);
+// const five = new BinaryTreeNode(5);
+// const six = new BinaryTreeNode(6);
+// const seven = new BinaryTreeNode(7);
+// const eight = new BinaryTreeNode(8);
+// const nine = new BinaryTreeNode(9);
+// const ten = new BinaryTreeNode(10);
+// const elvn = new BinaryTreeNode(11);
 // one.left = two;
 // one.right = three;
 // three.left = four;
