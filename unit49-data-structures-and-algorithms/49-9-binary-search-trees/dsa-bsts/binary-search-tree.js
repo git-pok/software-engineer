f36c241fbde4a5628cd1a7096f2ad0d37fd24895
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-07-07 17:04:24 -0400

    Edited binary-search-tree.js: started insert.

--- unit49-data-structures-and-algorithms/49-9-binary-search-trees/dsa-bsts/binary-search-tree.js
+++ unit49-data-structures-and-algorithms/49-9-binary-search-trees/dsa-bsts/binary-search-tree.js
@@ -15,7 +15,28 @@ class BinarySearchTree {
    * Returns the tree. Uses iteration. */
 
   insert(val) {
-
+    const queue = [this.root];
+    while (queue.length) {
+      const currNode = queue.shift();
+      console.log("CURR NODE", currNode);
+      if (this.root.val === undefined) {
+        this.root.val = val;
+        return this.root;
+      } else if (val <= currNode.val && currNode.left === null) {
+        const tree = new Node(val);
+        console.log("CURR NODE LEFT", currNode.left);
+        currNode.left = tree;
+      } else if (val >= currNode.val && currNode.right === null) {
+        console.log("CURR NODE RIGHT", currNode.right);
+        const tree = new Node(val);
+        currNode.right = tree;
+      } else if (val <= currNode.val && currNode.left !== null) {
+        queue.push(currNode.left);
+      } else if (val >= currNode.val && currNode.right !== null) {
+        queue.push(currNode.right);
+      }
+    }
+    return this.root;
   }
 
   /** insertRecursively(val): insert a new node into the BST with value val.
