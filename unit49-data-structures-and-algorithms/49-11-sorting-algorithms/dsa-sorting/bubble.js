053b5b89bb3e821d14f30f21c733f98a5e923f8e
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-07-13 16:18:01 -0400

    Edited bubble.js: completed bubbleSort.

--- unit49-data-structures-and-algorithms/49-11-sorting-algorithms/dsa-sorting/bubble.js
+++ unit49-data-structures-and-algorithms/49-11-sorting-algorithms/dsa-sorting/bubble.js
@@ -1,3 +1,19 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+    let count = 0;
+    for (let val = 0; val < arr.length; val++) {
+        let swapped = false;
+        for (let valItr = 0; valItr < arr.length - val; valItr++) {
+            console.log(arr);
+            count++;
+            if (arr[valItr] > arr[valItr + 1]) {
+                [arr[valItr + 1], arr[valItr]] = [arr[valItr], arr[valItr + 1]];
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    }
+    // console.log("TOTAL COUNT", count);
+    return arr;
+}
 
 module.exports = bubbleSort;
