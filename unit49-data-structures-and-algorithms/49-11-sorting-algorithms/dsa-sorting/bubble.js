ad6c9b8adaf887ba3b0e0ec91adf6d3c23b480d4
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2023-07-13 17:21:09 -0400

    Edited merge.js: completed merge and mergeSort.

--- unit49-data-structures-and-algorithms/49-11-sorting-algorithms/dsa-sorting/bubble.js
+++ unit49-data-structures-and-algorithms/49-11-sorting-algorithms/dsa-sorting/bubble.js
@@ -3,7 +3,8 @@ function bubbleSort(arr) {
     for (let val = 0; val < arr.length; val++) {
         let swapped = false;
         for (let valItr = 0; valItr < arr.length - val; valItr++) {
-            console.log(arr);
+            // console.log(arr);
+            console.log(arr[valItr], arr[valItr + 1]);
             count++;
             if (arr[valItr] > arr[valItr + 1]) {
                 [arr[valItr + 1], arr[valItr]] = [arr[valItr], arr[valItr + 1]];
--- unit49-data-structures-and-algorithms/49-11-sorting-algorithms/dsa-sorting/merge.js
+++ unit49-data-structures-and-algorithms/49-11-sorting-algorithms/dsa-sorting/merge.js
@@ -1,5 +1,37 @@
-function merge() {}
+function merge(arr1, arr2) {
+    let arr1Ptr = 0;
+    let arr2Ptr = 0;
+    const mergedArr = [];
 
-function mergeSort() {}
+    while (arr1Ptr < arr1.length && arr2Ptr < arr2.length) {
+        if (arr1[arr1Ptr] < arr2[arr2Ptr]) {
+            mergedArr.push(arr1[arr1Ptr]);
+            arr1Ptr++;
+        } else {
+            mergedArr.push(arr2[arr2Ptr]);
+            arr2Ptr++;
+        }
+    }
+
+    while (arr1Ptr < arr1.length) {
+        mergedArr.push(arr1[arr1Ptr]);
+        arr1Ptr++;
+    }
+
+    while (arr2Ptr < arr2.length) {
+        mergedArr.push(arr2[arr2Ptr]);
+        arr2Ptr++;
+    }
+
+    return mergedArr;
+}
+
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+    const mid = Math.floor(arr.length / 2);
+    const left = mergeSort(arr.slice(0, mid));
+    const right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+}
 
 module.exports = { merge, mergeSort};
