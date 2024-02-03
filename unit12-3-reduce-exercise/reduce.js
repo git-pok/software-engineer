da31bcdb05c6cbee5c6739a2f6fa269ef76b7fa5
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2024-02-02 22:17:28 -0500

    Edited unit12-3-reduce-exercise/reduce.js, re coded functions.

--- unit12-3-reduce-exercise/reduce.js
+++ unit12-3-reduce-exercise/reduce.js
@@ -17,7 +17,11 @@ function extractValue(arr, key) {
 };
 
 /*
-Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
+Write a function called vowelCount which accepts a string
+and returns an object with the keys as the vowel
+and the values as the number of times the vowel
+appears in the string. This function should be case
+insensitive so a lowercase letter and uppercase letter should count
 
 Examples:
     vowelCount('Elie') // {e:2,i:1};
@@ -29,24 +33,23 @@ Examples:
 
 function vowelCount(str) {
     const vowels = 'aeiou';
-    let newStr = str.split('');
-    return newStr.reduce(function(accu, next) {
-        let lowerCased = next.toLowerCase();
-        if (vowels.indexOf(lowerCased) !== -1) {
-            if (!accu[next]) {
-                accu[next] = 1;
-            } else {
-                accu[next] ++;
-            }
-            // // obj[next] = 1;
-            // console.log(obj);
+    let lwrCaseStr = str.toLowerCase().split('');
+    const vowelObj = lwrCaseStr.reduce((acc, next) => {
+        const isVowel = vowels.includes(next);
+        if (isVowel) {
+            if (!acc[next]) acc[next] = 1;
+            else acc[next] ++;
         }
-        return accu;
+        return acc;
     }, {});
+    return vowelObj;
 }
 
 /*
-Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
+Write a function called addKeyAndValue which accepts
+an array of objects and returns the array of objects
+passed to it with each object now including the key and
+value passed to the function.
 
 Examples:
     const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}];
@@ -61,41 +64,46 @@ Examples:
 */
 
 function addKeyAndValue(arr, key, value) {
-    return arr.reduce(function(accu, next, idx) {
-        accu[idx][key] = value;
+    return arr.reduce((acc, next, idx) => {
+        acc[idx][key] = value;
         return arr;
     }, arr);
 };
 
 /*
-Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
+Write a function called partition which accepts an
+array and a callback and returns an array with two
+arrays inside of it. The partition function should run
+the callback function on each value in the array and
+if the result of the callback function at that specific
+value is true, the value should be placed in the first subarray.
+If the result of the callback function at that specific value
+is false, the value should be placed in the second subarray. 
 
 Examples:
     
     function isEven(val){
         return val % 2 === 0;
     }
-    
+
     const arr = [1,2,3,4,5,6,7,8];
-    
+
     partition(arr, isEven) // [[2,4,6,8], [1,3,5,7]];
-    
+
     function isLongerThanThreeCharacters(val){
         return val.length > 3;
     }
-    
+
     const names = ['Elie', 'Colt', 'Tim', 'Matt'];
-    
+
     partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
 */
 
 function partition(arr, callback) {
-    return arr.reduce(function(accu, next) {
-        if (callback(next)) {
-            accu[0].push(next);
-        } else {
-            accu[1].push(next);
-        };
-        return accu;
+    return arr.reduce((acc, next) => {
+        const isCallbackTrue = callback(next);
+        if (isCallbackTrue) acc[0].push(next);
+        else acc[1].push(next);
+        return acc;
     }, [[], []]);
 };
