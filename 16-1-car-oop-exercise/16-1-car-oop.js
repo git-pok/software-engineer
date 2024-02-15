7d244a06a8b8c9f0ae2977318401454324ea8252
Vincent Impreveduto <vincent.impreveduto@icloud.com>
2024-02-15 13:34:23 -0500

    Re coded 16-1-car-oop-exercise/16-1-car-oop.js.

--- 16-1-car-oop-exercise/16-1-car-oop.js
+++ 16-1-car-oop-exercise/16-1-car-oop.js
@@ -6,18 +6,19 @@ class Vehicle {
         this.year = year;
     }
 
-    honk() {
+    honk () {
         return 'Beep.';
-    };
+    }
 
-    toString() {
-        return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
-    };
+    toString () {
+        const { make, model, year } = this;
+        return `The vehicle is a ${make} ${model} from ${year}`;
+    }
 }
 
 // PART 2 CREATE A CAR CLASS
 class Car extends Vehicle {
-    constructor(make, model, year) {
+    constructor (make, model, year) {
         super(make, model, year);
         this.numWheels = 4;
     }
@@ -25,32 +26,31 @@ class Car extends Vehicle {
 
 // PART 3 CREATE A MOTORCYCLE CLASS
 class Motorcycle extends Vehicle {
-    constructor(make, model, year) {
+    constructor (make, model, year) {
         super(make, model, year);
         this.numWheels = 2;
     }
 
-    revEngine() {
-        return 'VROOM!!!';
-    };
+    revEngine () {
+        return "VROOM!!!"
+    }
 }
 
 // PART 4 CREATE A GARAGE CLASS
 class Garage {
     constructor(capacity) {
-        this.capacity = capacity;
         this.vehicles = [];
-    } 
+        this.capacity = capacity;
+    }
 
-    add(newVehicle) {
-        if (!(newVehicle instanceof Vehicle)) {
-            return 'Only vehicles allowed in here!';
-        } 
-        if (this.vehicles.length >= this.capacity) {
-            return 'Sorry, we\'re full!';
-        }
-
-        this.vehicles.push(newVehicle);
-        return 'Vehicle addded!';
+    add (vehicleInstance) {
+        const { capacity, vehicles } = this;
+        const vehLen = vehicles.length;
+        const onlyVehcMsg = "Only vehicles are allowed in here!";
+        const isVehicleInst = vehicleInstance instanceof Vehicle;
+        if (!isVehicleInst) return onlyVehcMsg;
+        if (capacity === vehLen) return "Sorry, we're full.";
+        vehicles.push(vehicleInstance);
+        return "Vehicle added!"
     }
 }
